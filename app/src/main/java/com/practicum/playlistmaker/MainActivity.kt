d60a87e0d496fc68fc857c4f19d092dde6b4dc96
twistofthemyth <twist.of.the.myth@gmail.com>
2024-12-23 19:32:59 +0300

    ДЗ Работа в коде

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val searchButtonClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажата кнопка Поиск!!", Toast.LENGTH_LONG).show()
+            }
+        }
+        searchButton.setOnClickListener(searchButtonClickListener)
+
+        val mediaButton = findViewById<Button>(R.id.media_button)
+        mediaButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажата кнопка Медиатека!!", Toast.LENGTH_LONG).show()
+        }
+
+        attachDebugToast(findViewById<Button>(R.id.settings_button))
+    }
+
+    private fun attachDebugToast(view: TextView) {
+        view.setOnClickListener({
+            Toast.makeText(
+                this@MainActivity,
+                "Произошло нажатие на ${view.text}!!",
+                Toast.LENGTH_SHORT
+            ).show()
+        })
     }
 }
