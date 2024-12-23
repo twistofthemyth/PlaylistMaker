a202bf59af002d4df2bec0cfd78a701400098900
twistofthemyth <twist.of.the.myth@gmail.com>
2024-12-23 13:41:57 +0300

    ДЗ Верстка экрана

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.practicum.playlistmaker"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.practicum.playlistmaker"
