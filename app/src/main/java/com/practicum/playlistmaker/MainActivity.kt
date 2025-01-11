84efb4cb63c93382c6c9b3e8ab7c853101bf257b
twistofthemyth <twist.of.the.myth@gmail.com>
2025-01-11 16:53:54 +0300

    убрал временный код

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -14,24 +14,6 @@ class MainActivity : AppCompatActivity() {
         val mediaButton = findViewById<Button>(R.id.media_button)
         val settingsButton = findViewById<Button>(R.id.settings_button)
 
-        //TODO Убрать после прохождения ревью
-//        val searchButtonClickListener = object : View.OnClickListener {
-//            override fun onClick(v: View?) {
-//                Toast.makeText(this@MainActivity, "Нажата кнопка Поиск!!", Toast.LENGTH_LONG).show()
-//            }
-//        }
-//        searchButton.setOnClickListener(searchButtonClickListener)
-//
-//
-//        mediaButton.setOnClickListener {
-//            Toast.makeText(this@MainActivity, "Нажата кнопка Медиатека!!", Toast.LENGTH_LONG).show()
-//        }
-//
-//        settingsButton.setOnClickListener {
-//            Toast.makeText(this@MainActivity, "Нажата кнопка Настройки!!", Toast.LENGTH_SHORT)
-//                .show()
-//        }
-
         searchButton.setOnClickListener {
             val intent = Intent(this, SearchActivity::class.java)
             startActivity(intent)
