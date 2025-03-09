a8626160f2146b27c379d29b2fa51327c6d8b904
twistofthemyth <twist.of.the.myth@gmail.com>
2025-03-09 23:25:34 +0300

    расширил модель Track

--- app/src/main/java/com/practicum/playlistmaker/model/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/model/Track.kt
@@ -7,17 +7,35 @@ data class Track(
     val trackName: String,
     val artistName: String,
     val trackTime: String,
-    val artworkUrl100: String
+    val artworkUrl100: String,
+    val collectionName: String,
+    val releaseDate: String,
+    val primaryGenreName: String,
+    val country: String
 ) {
     constructor(
         trackName: String,
         artistName: String,
         trackTime: Long,
-        artworkUrl100: String
-    ) : this(trackName, artistName, TRACK_TIME_FORMATTER.format(trackTime), artworkUrl100)
+        artworkUrl100: String,
+        collectionName: String,
+        releaseDate: String,
+        primaryGenreName: String,
+        country: String
+    ) : this(
+        trackName,
+        artistName,
+        TRACK_TIME_FORMATTER.format(trackTime),
+        artworkUrl100,
+        collectionName,
+        releaseDate,
+        primaryGenreName,
+        country
+    )
 
     companion object {
-        private val TRACK_TIME_FORMATTER = SimpleDateFormat("mm:ss", Locale.getDefault()).format(293000L)
+        private val TRACK_TIME_FORMATTER =
+            SimpleDateFormat("mm:ss", Locale.getDefault()).format(293000L)
     }
 }
 
