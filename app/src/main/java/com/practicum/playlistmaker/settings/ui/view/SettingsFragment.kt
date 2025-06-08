27b232a2d3ce6d69adea71ac8bcb05bbf9a55a2e
twistofthemyth <twist.of.the.myth@gmail.com>
2025-06-09 01:59:50 +0300

    использование навбара

--- app/src/main/java/com/practicum/playlistmaker/settings/ui/view/SettingsFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/settings/ui/view/SettingsFragment.kt
@@ -18,14 +18,15 @@ import org.koin.androidx.viewmodel.ext.android.activityViewModel
 class SettingsFragment : Fragment() {
 
     private val viewModel: SettingsViewModel by activityViewModel<SettingsViewModel>()
-    private lateinit var binding: FragmentSettingsBinding
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentSettingsBinding.inflate(inflater)
+        _binding = FragmentSettingsBinding.inflate(inflater)
         return binding.root
     }
 
@@ -34,6 +35,11 @@ class SettingsFragment : Fragment() {
         setupInput()
         observeNavigation()
         observeThemeChange()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     private fun setupInput() {
