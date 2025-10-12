ead028a30e095336783351c7e76a5fb1c973f923
twistofthemyth <twist.of.the.myth@gmail.com>
2025-10-12 15:24:37 +0300

    перенес объявление зависимостей в libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("androidx.navigation.safeargs.kotlin")
-    id("kotlin-kapt")
-    id("org.jetbrains.kotlin.plugin.compose")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.androidx.navigation.safeargs)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
@@ -34,7 +34,7 @@ android {
         viewBinding = true
         compose = true
     }
-    
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -45,41 +45,35 @@ android {
 }
 
 dependencies {
-    val retrofitVersion = "2.11.0"
-    val navVersion = "2.9.3"
-    val roomVersion = "2.7.2"
-
     val composeBom = platform("androidx.compose:compose-bom:2025.09.01")
     implementation(composeBom)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.android.material)
+    implementation(libs.google.gson)
+    implementation(libs.retrofit2.retrofit)
+    implementation(libs.retrofit2.converter.gson)
+    implementation(libs.bumptech.glide)
+    implementation(libs.koin)
+    implementation(libs.kotlin.coroutines)
 
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.core", "core-ktx", "1.16.0")
-    implementation("androidx.appcompat", "appcompat", "1.7.1")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.activity:activity-compose:1.11.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
-    implementation("androidx.activity", "activity", "1.10.1")
-    implementation("androidx.constraintlayout", "constraintlayout", "2.2.1")
-    implementation("androidx.navigation", "navigation-fragment-ktx", navVersion)
-    implementation("androidx.navigation", "navigation-ui-ktx", navVersion)
-    implementation("androidx.room", "room-runtime", roomVersion)
-    implementation("androidx.room", "room-ktx", roomVersion)
-    implementation("com.google.android.material", "material", "1.12.0")
-    implementation("com.google.code.gson", "gson", "2.13.1")
-    implementation("com.squareup.retrofit2", "retrofit", retrofitVersion)
-    implementation("com.squareup.retrofit2", "converter-gson", retrofitVersion)
-    implementation("com.github.bumptech.glide", "glide", "4.16.0")
-    implementation("io.insert-koin", "koin-android", "3.3.0")
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-android", "1.10.2")
-
-    testImplementation("junit", "junit", "4.13.2")
-
-    androidTestImplementation("androidx.test.ext", "junit", "1.3.0")
-    androidTestImplementation("androidx.test.espresso", "espresso-core", "3.7.0")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.unit)
+    androidTestImplementation(libs.androidx.test.espresso)
     androidTestImplementation(composeBom)
 
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation(libs.androidx.compose.ui.tooling)
 
-    kapt("androidx.room", "room-compiler", roomVersion)
+    kapt(libs.androidx.room.compiler)
 }
